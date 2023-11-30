1cb6d8ac4807b243a65843b72ac5638c8a47e71f
Laura Robinson <laura3918@hotmail.co.uk>
2023-11-30 19:41:05 +0000

    feat: Now connects to database

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,4 +1,7 @@
-import { Sequelize } from "sequelize";
-
-// TODO: This should be external config
-export const sequelize = new Sequelize("sqlite::memory:");
+import { Dialect, Sequelize } from "sequelize";
+export let sequelize = new Sequelize("sqlite::memory:");
+import { CONFIG } from "../config";
+if (process.env.NODE_ENV !== "test") {
+	const connString = `postgres://${CONFIG.dbUserName}:${CONFIG.dbPassword}@${CONFIG.dbHost}/${CONFIG.dbName}`;
+	sequelize = new Sequelize(connString);
+}
